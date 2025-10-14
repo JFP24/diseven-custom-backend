25e108ca9c0695521a287ef5a691db6b35e75ae7
juan <workpulgarin@gmail.com>
2025-10-14 15:09:12 -0500

    nuevo

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import app from './app.js';
-//import connectDB from './db.js';
+// import connectDB from './db.js';
 
-const PORT = process.env.PORT || 3000;
+// Configuración sin .env
+const PORT = 3000;
+const HOST = '0.0.0.0';
 
-//connectDB();
+// connectDB();
 
-app.listen(PORT, () => {
-  console.log(`✅ Servidor corriendo en puerto ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`✅ Servidor corriendo en http://${HOST}:${PORT}`);
 });
